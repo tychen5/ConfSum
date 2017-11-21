ab4634472896ed05cf21f05eb35625b0e98ee1d7
Steven Wu <aaa33661@gmail.com>
2017-11-21 20:35:21 +0800

    11/21 20:35
    好像加上google sign in了

--- public/live.js
+++ public/live.js
@@ -35,7 +35,17 @@ window.onload=function(){
     buildElement();
     
     firebase.initializeApp(config);
-    
+    var provider = new firebase.auth.GoogleAuthProvider(); 
+    firebase.auth().signInWithPopup(provider).then(function(result) {      
+      var token         = result.credential.accessToken;      
+      var user          = result.user;      // 使用者資訊
+    }).catch(function(error) {
+      // 處理錯誤
+      var errorCode     = error.code;
+      var errorMessage  = error.message;     
+      var email         = error.email;      // 使用者所使用的 Email
+      var credential    = error.credential;      
+    });
     buildListener();
     
     showdate();
