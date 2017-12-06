294ef3ca1eebf9974222a38e2f384974765103a1
Steven Wu <aaa33661@gmail.com>
2017-12-06 14:08:10 +0800

    2017/12/06 14:07
    user info test

--- public/live.js
+++ public/live.js
@@ -41,6 +41,7 @@ window.onload=function(){
       var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
+        window.alert(user);
       // ...
     }).catch(function(error) {
       // Handle Errors here.
