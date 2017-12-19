98eebd26b0f356d0ccd6446f383b3bc1eb09cd63
tychen5 <r06725035@g.ntu.edu.tw>
2017-12-19 16:01:42 +0800

    FB sign in

--- public/live.js
+++ public/live.js
@@ -258,6 +258,36 @@ function googleSignout() {
       console.log('Signout Failed')  
    });
 }
+function FBSignin(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  //var user = result.user;
+
+    var user = firebase.auth().currentUser;
+      // The signed-in user info.
+      //var user = result.user;
+      var user2 =user.displayName;
+        window.alert("Welcome:"+user2);
+
+
+
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
+}
 function getAccountPermission(){
     var checkpassword=""; firebase.database().ref('users/'+id.value+"/Password").once("value").then(function(snapshot){
          checkpassword=snapshot.val();
