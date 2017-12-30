dcf7938ac6bb047245dec7725afa9b08bc37463b
Steven Wu <aaa33661@gmail.com>
2017-12-30 22:30:13 +0800

    12/30 22:30
    修正監聽事件的bug

--- public/live.js
+++ public/live.js
@@ -255,24 +255,7 @@ function readSubtitle(id_string){//讀取所有字幕
     var timestamp;
     //allsubref = firebase.database().ref('users/'+id_string+"/RecordTitle/"+final_title+'/Subtitle');
     allsubref = firebase.database().ref('test/Subtitle/'+roomNumber+'/');
-    allsubref.onChildAdded(DataSnapshot snapshot, String s){
-        for(var i in snapshot.val()){
-          
-              
-          console.log("這是console.log:"+i.name+".id");
-        
-          string=JSON.stringify(snapshot.val()[i].record_perSentence);
-                    
-          text = document.createTextNode(snapshot.val()[i].time+snapshot.val()[i].name+"說:"+string.substr(1,string.length-2)+"\n");
-          textarea.append(text);
-            if(textarea.selectionStart == textarea.selectionEnd) {
-            textarea.scrollTop = textarea.scrollHeight;
-            }
-                
-          
-        }
-    }
-    /*allsubref.limitToLast(1).on('value', function(snapshot        
+    allsubref.limitToLast(1).on('child_added', function(snapshot        
       for(var i in snapshot.val()){
           
               
@@ -290,7 +273,7 @@ function readSubtitle(id_string){//讀取所有字幕
         }
         console.log("這是console.log(textarea.value):"+textarea.value);
         
-    });*/
+    });
     
     
 }
