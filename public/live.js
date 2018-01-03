49617f362d90cdb73022b18513f4504d6d45fb38
Steven Wu <aaa33661@gmail.com>
2018-01-03 14:01:58 +0800

    01/03 14:01

--- public/live.js
+++ public/live.js
@@ -434,9 +434,10 @@ function confirmNewAccount(){
         else{            
             firebase.auth().createUserWithEmailAndPassword(id.value.toString(), password.value.toString()).catch(function(error) {
                 // Handle Errors here.
-                window.alert("成功創立帳號");
+                //window.alert("成功創立帳號");
                 var errorCode = error.code;
                 var errorMessage = error.message;
+                SignInWithMail();
                 // ...
             });
             
